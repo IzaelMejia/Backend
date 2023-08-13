20b661538876766d911edcecf6bd301e8ebd6672
Izael Mejia <izaelmejiaa@gmail.com>
2023-08-12 19:58:02 -0600

    actualizacion

--- main.js
+++ main.js
@@ -1,72 +1,78 @@
+import {promises as fs } from "fs"
+
 //1-Clase ProductManager
 class ProductManager{
-    constructor(){
+    constructor(path){
         //2-arreglo vacío
         this.productos= []
-        this.ultimoId = 0; // Inicializamos el contador del último ID utilizado en 0
+        this.ultimoId = 10; // Inicializamos el contador del último ID utilizado en 0
+        this.path = "./productos.json"
     }
 
     //3-Parametros a pedir
     //4-metodo agregar productos
-    addProduct(titulo, descripcion, precio, ruta, code, stock) {
-        //  Comprobar si el código ya se utiliza para otro producto
-        if (this.productos.some((producto) => producto.code === code)) {
+    addProduct= async(producto)=>{
+        const productList = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const product = productList.find((prod) => prod.id === producto.id);
+
+        if (product) {
             console.log("Código repetido, no se agregó el producto.");
             return false; //  Devuelve falso si el código está duplicado para indicar que el producto no se agregó
         }
         // Incrementar el contador de ID y asignarlo al nuevo producto
         this.ultimoId++;
-        this.productos.push({ id: this.ultimoId, titulo, descripcion, precio, ruta, code, stock });
+        productList.push({ id: this.ultimoId, producto });
+        await fs.writeFile(this.path, JSON.stringify(productList))
         return true; // Devuelve verdadero para indicar que el producto se agregó con éxito
     }
 
     //5-Metodo mostrar productos del array
-    getProduct(){
-        return this.productos
+    
+
+    getProduct= async()=>{
+        const productList = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        return productList;
     }
 
     //6-metodo obteer prodcto por id
-    getProductId(id){
+    getProductId= async(id)=>{
         //si el producto que buscamos en array productos se encuetra el producto que sea igual al id que pasamos ...
-        const product = this.productos.find((producto) => producto.id === id);
+        const productList = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const product = productList.find((producto) => producto.id === id);
+        
         if (product) { //si el producto que buscamos en el array es el producto con id igual al id que pide el usuario
-            console.log("Si existe el producto:", product.titulo);  
+            console.log("Si existe el producto:", product);  
             return product; // retornar el objeto del producto
 
         }else{      
             console.log("No existe ese producto o id");
         }
-    }Z
+    }
 
 
-    getUpdateProduct(id, updatedProduct) {
-        // Buscar el producto en el array por su ID
-        const productIndex = this.productos.findIndex((producto) => producto.id === id);
+    getUpdateProduct= async(id, updatedProduct)=> {
+        const productList = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const productIndex = productList.findIndex((producto) => producto.id === id);
 
         if (productIndex !== -1) {
             // Actualizar las propiedades del producto
-            this.productos[productIndex] = { ...this.productos[productIndex], ...updatedProduct };
-            console.log("Producto actualizado:", this.productos[productIndex].titulo);
-            return true;
+            productList[productIndex].titulo = updatedProduct.titulo
+            await fs.writeFile(this.path, JSON.stringify(productList))
+            
         } else {
             console.log("No existe el producto con ese ID:", id);
-            return false;
         }
     }
 
 
-    deleteProduct(id) {
-        // Buscar el índice del producto en el array por su ID
-        const productIndex = this.productos.findIndex((producto) => producto.id === id);
+    deleteProduct=async(id)=> {
+        const productList = JSON.parse(await fs.readFile(this.path, "utf-8"))
+        const productIndex = productList.find(prod => prod.id === id);
 
-        if (productIndex !== -1) {
-            // Eliminar el producto del array usando splice
-            const deletedProduct = this.productos.splice(productIndex, 1);
-            console.log("Producto eliminado:", deletedProduct[0].titulo);
-            return true;
+        if (productIndex ) {
+            await fs.writeFile(this.path, JSON.stringify(productList.filter(prod => prod.id != id)))
         } else {
             console.log("No existe el producto con ese ID:", id);
-            return false;
         }
     }
     
@@ -75,42 +81,28 @@ class ProductManager{
 //Iniciamos Pruebas
 
 const productos = new ProductManager
-//Mostrar producto (no hay nada)
-console.log("Llamada getProducts sin datos:");
-console.log(productos.getProduct());
 
-console.log("--------------------");
+//Forma 1 de ver productos
+console.log(await productos.getProduct());
 
-//agregar producto 
-productos.addProduct("producto prueba","Este es un producto prueba",200,"Sin imagen","abc123",25)
-productos.addProduct("producto prueba2","Este es un producto prueba2",200,"Sin imagen2","abc1234",25) 
-productos.addProduct("producto prueba3","Este es un producto prueba3",200,"Sin imagen3","abc1234",25)  //este es el code repetido
-
-//Mostrar producto 
-console.log("--------------------");
-console.log(productos.getProduct());
-
-console.log("--------------------");
-//Buuscar producto por Id
-productos.getProductId(2); //si existe id
-productos.getProductId(4); //no existe id 
+//Forma 2
+// async function verProductos() {
+//     const productList = await productos.getProduct();
+//     console.log(productList);
+// }
+// verProductos();
 
 
-console.log("--------------------");
 
-// Pruebas para actualizar un producto
-productos.getUpdateProduct(2, {
-    titulo: "Producto actualizado",
-    descripcion: "Esta es la nueva descripción",
-    precio: 250,
-    ruta: "Nueva imagen",
-    code: "xyz789",
-    stock: 30,
-});
-
-console.log("--------------------");
-console.log(productos.getProduct());
+ //agregar producto 
+//  const producto1={titulo:"Shampu Gatito", descripcion: "Shampu 200ml", precio:200, ruta:"Sin imagen", code:"sadsd", stock:20}
+//  productos.addProduct(producto1)
 
 
-productos.deleteProduct(2); // Elimina el producto con ID 2
-console.log(productos.getProduct()); // Muestra los productos restantes en el array
+ // Pruebas para actualizar un producto
+// productos.getUpdateProduct(2, {
+//     titulo: "Producto actualizado",
+// });
+
+// productos.deleteProduct(11)
+
